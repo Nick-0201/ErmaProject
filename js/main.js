b45ea6e8a1b59299f95d3c0e641be0c13130c018
Nick-0201 <nurmukhammad93@icloud.com>
2023-08-15 08:56:27 +0600

    mobailmenu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+// --------------------header blur--------------------
+
 const header = document.querySelector("header")
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 60)
+  header.classList.toggle("sticky", window.scrollY > 60);
 });
+
+// --------------------header humburger--------------------
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.header__menu');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
+};
